b5d85ce966d5be4c74a4276bce3f60fa45dcc63b
Gerardo Cano <gcano639@gmail.com>
2023-02-25 14:52:34 -0600

    creacion de clases

---
+++ src/proyectoprogra2_gcano/Usuario.java
@@ -0,0 +1,49 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package proyectoprogra2_gcano;
+
+/**
+ *
+ * @author gcano
+ */
+public class Usuario {
+    protected String password, user, rol;
+
+    public Usuario(String password, String user, String rol) {
+        this.password = password;
+        this.user = user;
+        this.rol = rol;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getUser() {
+        return user;
+    }
+
+    public void setUser(String user) {
+        this.user = user;
+    }
+
+    public String getRol() {
+        return rol;
+    }
+
+    public void setRol(String rol) {
+        this.rol = rol;
+    }
+
+    @Override
+    public String toString() {
+        return user;
+    }
+    
+}
